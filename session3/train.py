6931e0e151fd26cb13d7ca167690c38e06fc44f3
Girish Kumar <GK@example.com>
2025-01-13 18:57:20 +0530

    addded linter config

--- session3/train.py
+++ session3/train.py
@@ -4,21 +4,19 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+
 data = load_iris()
 
-X = pd.DataFrame(data.data,columns=data.feature_names)
-y =  pd.DataFrame(data.target,columns=['target'])
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = pd.DataFrame(data.target, columns=['target'])
 
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
 model = RandomForestClassifier()
-model.fit(X_train,y_train.values.ravel())
+model.fit(X_train, y_train.values.ravel())
 
 print('saving Model')
 
-joblib.dump(model,'model.joblib')
-
-
-
+joblib.dump(model, 'model.joblib')
